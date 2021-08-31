66671569f77909caa60ccdcf5f7a6843fbd367f1
kevosalaz <kevin.sala94@gmail.com>
2021-08-30 20:00:33 -0600

    Commit de proyecto criptomonedas

---
+++ Dapper.Infrastructure/Repositories/UsuarioRepository.cs
@@ -0,0 +1,78 @@
+﻿using Dapper.Application.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using Dapper.Core.Entities;
+
+namespace Dapper.Infrastructure.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly IConfiguration configuration;
+        public UsuarioRepository(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public async Task<int> AddAsync(Usuario entity)
+        {
+            //entity.Fecharegistro = DateTime.Now;
+            var sql = "INSERT INTO dbo.usuario (email, password, fecharegistro) VALUES (@Email, @Password, @Fecharegistro)";
+            using (var connection = new SqlConnection(configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                var result = await connection.ExecuteAsync(sql, entity);
+                return result;
+            }
+        }
+
+        public async Task<int> DeleteAsync(int id)
+        {
+            var sql = "DELETE FROM dbo.usuario WHERE idusuario = @IdUsuario";
+            using (var connection = new SqlConnection(configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                var result = await connection.ExecuteAsync(sql, id);
+                return result;
+            }
+        }
+
+        public async Task<IReadOnlyList<Usuario>> GetAllAsync()
+        {
+            var sql = "SELECT * FROM dbo.usuario ORDER BY idusuario";
+            using (var connection = new SqlConnection(configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                var result = await connection.QueryAsync<Usuario>(sql);
+                return result.ToList();
+            }
+        }
+
+        public async Task<Usuario> GetByIdAsync(int id)
+        {
+            var sql = "SELECT * FROM dbo.usuario WHERE idusuario = @IdUsuario";
+            using (var connection = new SqlConnection(configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                var result = await connection.QuerySingleOrDefaultAsync<Usuario>(sql, new { IdUsuario = id });
+                return result;
+            }
+        }
+
+        public async Task<int> UpdateAsync(Usuario entity)
+        {
+            var sql = "UPDATE dbo.usuario SET email = @Email, password = @Password, fecharegistro = @Fecharegistro WHERE idusuario = @IdUsuario";
+            using (var connection = new SqlConnection(configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                var result = await connection.ExecuteAsync(sql, entity);
+                return result;
+            }
+        }
+    }
+}
